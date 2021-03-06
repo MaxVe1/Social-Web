4204b6c0c3ee1e321e6efca18030c62ff21bc2a0
maxve1 <emvi@tut.by>
2021-03-06 11:44:30 +0300

    54 ComponentDidMount added to Users Comp

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -12,14 +12,14 @@ type UsersPropsType = {
 };
 
 export class Users extends React.Component<UsersPropsType> {
-    constructor(props: UsersPropsType) {
-        super(props);
-
+    componentDidMount() {
         axios.get("https://social-network.samuraijs.com/api/1.0/users").then((response) => {
             this.props.setUsers(response.data.items);
         });
     }
-
+    componentDidUpdate() {
+        console.log("Did Update")
+    }
     render() {
         return (
             <div>
